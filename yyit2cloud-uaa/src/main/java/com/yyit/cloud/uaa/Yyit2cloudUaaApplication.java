c51f01fe5c787dd9daab11c23339b55d03c8c17c
yanpingping <yanpp@nfky.com>
2022-10-14 10:08:41 +0800

    add: create new project for uua

---
+++ yyit2cloud-uaa/src/main/java/com/yyit/cloud/uaa/Yyit2cloudUaaApplication.java
@@ -0,0 +1,13 @@
+package com.yyit.cloud.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yyit2cloudUaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yyit2cloudUaaApplication.class, args);
+	}
+
+}
